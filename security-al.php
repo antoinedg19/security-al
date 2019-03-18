ecb44fb20434d539eb0c229c347ada8bfeb0b57d
antoinedg19 <antoinedg19@gmail.com>
2019-03-18 18:16:13 +0100

    Update security-al.php

--- security-al.php
+++ security-al.php
@@ -1,14 +1,14 @@
 <?php 
 /*
 Plugin Name: Security Al
-Plugin URI: https://aldigirolamo.fr
+Plugin URI: https://github.com/antoinedg19/security-al
 Description: This is a plugin for security info initialy in the function.php
 Author: Al Di Girolamo
 Version: 1.3
 Author URI: https://aldigirolamo.fr
 */
 
-// Suppression du login de l'auteur dans les commentaires
+// Deleting author name in the comment section
 function remove_comment_author_class( $classes ) {
     foreach( $classes as $key => $class )
      if(strstr($class, 'comment-author-' ))
@@ -16,18 +16,18 @@ function remove_comment_author_class( $classes ) {
     return $classes;
    }
    add_filter( 'comment_class' , 'remove_comment_author_class' );
-   // Désactivation des infos d'erreur de login
+   // Deleting the error infos from the login page
    function remove_login_error_msg() {
     return 'Erreur';
    }
    add_filter( 'login_errors', 'remove_login_error_msg' );
-   // Désactivation XMLRPC
+   // Desactivate the XMLRPC.php
    add_filter('xmlrpc_enabled', '__return_false');
    remove_action('wp_head', 'rsd_link');
-   // Suppression de la version de WordPress
+   // Deleting Wp version
    remove_action('wp_head', 'wp_generator');
    remove_action('wp_head', 'wlwmanifest_link');
-   //Supression generateur flux RSS 
+   //Deleting rss flux
    add_filter('get_the_generator_rss2', '__return_false');
    add_filter('get_the_generator_atom', '__return_false');
    // Suppression version dans flux RSS
@@ -35,7 +35,7 @@ function remove_comment_author_class( $classes ) {
     return '';
    }
    add_filter('the_generator', 'remove_version_wp');
-   // Suppression des versions des fichiers css/js inclus
+   // Deleting wp version in the rss flux
    function remove_ver_css_js( $src ) {
     if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
      $src = remove_query_arg( 'ver', $src );
