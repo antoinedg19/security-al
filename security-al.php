1206b8de20fde22b6bd43620efa65625dde916c3
antoinedg19 <antoinedg19@gmail.com>
2019-03-19 15:27:02 +0100

    Update security-al.php

--- security-al.php
+++ security-al.php
@@ -8,34 +8,45 @@ Version: 1.5
 Author URI: https://aldigirolamo.fr
 */
 
+//Remove Element From Head
+  function remove_header_info() {
+    remove_action('wp_head', 'feed_links_extra', 3);
+    remove_action('wp_head', 'rsd_link');
+    remove_action('wp_head', 'wlwmanifest_link');
+    remove_action('wp_head', 'wp_generator');
+  }
+  add_action('init', 'remove_header_info');
+
 // Deleting author name in the comment section
-function remove_comment_author_class( $classes ) {
+  function remove_comment_author_class( $classes ) {
     foreach( $classes as $key => $class )
      if(strstr($class, 'comment-author-' ))
       unset( $classes[$key] );
     return $classes;
    }
    add_filter( 'comment_class' , 'remove_comment_author_class' );
-   // Deleting the error infos from the login page
+
+// Deleting the error infos from the login page
    function remove_login_error_msg() {
     return 'Erreur';
    }
-   add_filter( 'login_errors', 'remove_login_error_msg' );
-   // Desactivate the XMLRPC.php
-   add_filter('xmlrpc_enabled', '__return_false');
-   remove_action('wp_head', 'rsd_link');
-   // Deleting Wp version
-   remove_action('wp_head', 'wp_generator');
-   remove_action('wp_head', 'wlwmanifest_link');
-   //Deleting rss flux
+  add_filter( 'login_errors', 'remove_login_error_msg' );
+
+// Desactivate the XMLRPC.php
+  add_filter( 'wp_xmlrpc_server_class', '__return_false' );
+  add_filter('xmlrpc_enabled', '__return_false');
+
+//Deleting rss flux
    add_filter('get_the_generator_rss2', '__return_false');
    add_filter('get_the_generator_atom', '__return_false');
-   // Suppression version dans flux RSS
+
+// Suppression version dans flux RSS
    function remove_version_wp() {
     return '';
    }
    add_filter('the_generator', 'remove_version_wp');
-   // Deleting wp version in the rss flux
+
+// Deleting wp version in enqueud script
    function remove_ver_css_js( $src ) {
     if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) )
      $src = remove_query_arg( 'ver', $src );
